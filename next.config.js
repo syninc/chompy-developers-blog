742503194737a302df6824c8810e0988d51abfa0
tyapama <tyapama@users.noreply.github.com>
2022-06-02 10:50:01 +0000

    deploy: 627dba2b11eaf5aef2b42fb75bbcbcb43c2de25f

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const isProd = process.env.NODE_ENV === "production";
-
-module.exports = {
-    basePath: isProd ? "/chompy-developers-blog" : "",
-    assetPrefix: isProd ? "/chompy-developers-blog/" : "",
-    trailingSlash: true
-};
--- src/components/LinkBackHome.tsx
+++
@@ -1,7 +0,0 @@
-import Link from "next/link";
-
-export const LinkBackHome: React.FC = () => (
-  <Link href="/" passHref>
-    <a className="link-back-home">Back Home</a>
-  </Link>
-);
